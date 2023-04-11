5e6a6b84e590dd7ed979c4488f3d8232018091cf
Arthur Silva <53120477+arthurmsilva@users.noreply.github.com>
2023-04-11 17:40:53 -0300

    Exemplo 0 - Primeiro projeto

---
+++ exemplo0.py
@@ -0,0 +1,21 @@
+# Exemplo 0 - Primeiro projeto
+
+import streamlit as st
+
+st.title('Meu primeiro projeto no Streamlit')
+
+st.write('Realizando testes com Rerun...')
+
+st.markdown('''
+
+# Colocando um título com markdown
+
+**negrito**, *itálico*
+
+## Subtitulo
+
+''')
+
+st.image('https://via.placeholder.com/500x400/')
+
+
