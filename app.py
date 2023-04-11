d068ed7d632b2aaa8ef7f1f40fad5f6bebd864f5
Arthur Silva <53120477+arthurmsilva@users.noreply.github.com>
2023-04-11 17:24:38 -0300

    Colocando o projeto spacy

--- app.py
+++ app.py
@@ -1,30 +1,69 @@
-# Exemplo 01 - Criar um visualizador de arquivos com 30 linhas
+########## Exemplo 03 - Aprender a utilizar os layouts ###########
 
 import streamlit as st
-import pandas as pd
+from spacy import load, displacy
+from streamlit.components import v1 as components # gerar renderização html
 
-st.markdown(
-'''
-# Exibidor de arquivos
+# Carregando as informações da análise de texto
+nlp = load('pt_core_news_lg')
 
-## Suba um arquivo e vejamos o que acontece:
-'''
+# Criando um sidebar (Barra lateral)
+bar = st.sidebar
+
+# Criando uma barra de escolha a ser colocada no sidebar
+escolha = bar.selectbox(
+    'Escolha uma categoria',
+    ['Entidades', 'Gramática']
 )
 
-arquivo = st.file_uploader (
-    'Suba seu arquivo aqui',
-    type=['jpg','png','csv','py','xlsx','json']
-          )
+# Criando uma barra de texto longo
+text = st.text_area('Escreva um texto longo:')
 
-if arquivo:
-    print(arquivo.type)
-    match arquivo.type.split('/'):
-        case 'image', _:
-            st.image(arquivo)
-        case 'text', 'x-python':
-            st.code(arquivo.read().decode())
-        case 'text', 'csv':
-            df = pd.read_csv(arquivo)
-            st.dataframe(df)
-else: st.error("Ainda não tenho arquivo")
+# Realizando a análise do texto
+doc = nlp(text)
+
+# Se escolha Entidade, dividir em duas colunas apresentando as entidades e o tipo das mesmas
+if text and escolha == 'Entidades':
+    data = displacy.render(doc, style='ent') # spacy gera um html
+    
+    with st.expander('Dados do spacy'):
+        components.html(
+            data,
+            scrolling=True,
+            height=300
+        )
+    a, b = st.columns(2)
+    for e in doc.ents:
+        a.info(e)
+        b.warning(e.label_)
+
+# Se escolha Gramática, aparece um filtro adicional onde o usuário seleciona o tipo gramático
+## Verbo, pronome, adverbio, etc
+if text and escolha == 'Gramática':
+    filtro = bar.multiselect(
+        'Filtro',
+        ['VERB', 'PROPN', 'ADV', 'AUX'],
+        default=['VERB', 'PROPN']
+        )
+    # Apresentando os dados json
+    with st.expander('Dados do spacy'):
+        st.json(doc.to_json())
+    
+    # Criação de containers para apresentar mais de uma info ao mesmo tempo
+    container = st.container()
+    
+    a,b,c = container.columns(3) # Definindo as três colunas
+
+    a.subheader('Token')
+    b.subheader('Tag')
+    c.subheader('Morph')
+    
+    # Loop para criar a estrutura para cada palavra 
+    for t in doc:
+        if t.tag_ in filtro:
+            container = st.container() # Novo container somente para organizar os dados
+            a,b,c = container.columns(3)
             
+            a.info(t)
+            b.warning(t.tag_)
+            c.error(t.morph)
